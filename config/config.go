6164e06eff07d9c23f11b5ab189555348b33a9f1
Denis <deniswilliam@luizalabs.com>
2019-01-08 11:11:26 -0200

    config

--- config/config.go
+++ config/config.go
@@ -17,10 +17,10 @@ type Config struct {
 // Configuracoes centraliza as opções da api
 func Configuracoes() Config {
 	r := Config{
-		PageAccessToken: os.Getenv("PAGEACCESSTOKEN"), //"Bearer DQVJ0b3V3ZAGpZAUEs5blZAPZA0I0S2MyeVdlLThSYnhoenFFV0V6SkJ0YU9UR01aNWgyUHBONDNkUmRLLXFqQWRsODBHMDdHWjNURE9WNi00SWNWNWNMaHRxUi1TaU5vNGV1U3o0Mk9DZAVpockcxZATV5M20tcXBwZAEdKM3ZAxUXh4cHNZAVl9xdXAxTDdPTGdFSThYWlo3eFNvYzBqODRHS201WlFoWjY0ckt4cjE5VXIwYmxjQ3hiLU5wZAC1yYmVOUjh6dkdMOFVB",
-		VerifyToken:     os.Getenv("VERIFYTOKEN"),     //"JARVIS",
-		AppSecret:       os.Getenv("APPSECRET"),       //"def44fb9938372b6bade48a93b8f91dc",
-		GraphURL:        os.Getenv("GRAPHURL")}        //"https://graph.facebook.com/v2.6/"
+		PageAccessToken: os.Getenv("PAGEACCESSTOKEN"),
+		VerifyToken:     os.Getenv("VERIFYTOKEN"),
+		AppSecret:       os.Getenv("APPSECRET"),
+		GraphURL:        os.Getenv("GRAPHURL")}
 
 	return r
 }
